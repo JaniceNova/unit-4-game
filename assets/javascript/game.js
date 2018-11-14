18a0bd4654d054ede36779ec10c1651db0f4eaf8
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-13 22:13:04 -0800

    setting up wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,8 +19,10 @@ console.log($("#c3").valueAsNumber =crystalNumber3);
 console.log($("#c4").valueAsNumber =crystalNumber4);
 
 var zero = 0;
-
+var winsStartAtZero = 0;
+var losesStartAtZero = 0;
 //buttons value appear in dark green box and add
+
 function Function1() { 
 
     $("#secondgreenbox").html(zero + crystalNumber1);
@@ -28,12 +30,59 @@ function Function1() {
     console.log($("#c1").valueAsNumber =crystalNumber1);
 
     zero = zero + crystalNumber1;
+
+    if(zero === numberComputerPicked) {
+        zero = 0;
+       $("#winScore").html(winsStartAtZero + 1);
+       winsStartAtZero = winsStartAtZero + 1;
+       console.log("hello")
+
+
+       numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+
+       $(".lightgreenbox").html(numberComputerPicked);
+
+        crystalNumber1 = Math.floor((Math.random() * 12) + 1);
+ crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+ crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+ crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+$("#c1").valueAsNumber =crystalNumber1;
+$("#c2").valueAsNumber =crystalNumber2;
+$("#c3").valueAsNumber =crystalNumber3;
+$("#c4").valueAsNumber =crystalNumber4;
+console.log($("#c1").valueAsNumber =crystalNumber1);
+console.log($("#c2").valueAsNumber =crystalNumber2);
+console.log($("#c3").valueAsNumber =crystalNumber3);
+console.log($("#c4").valueAsNumber =crystalNumber4);
+    }
+    if(zero > numberComputerPicked) {
+   ("#loseScore").html(losesStartAtZero + 1);
+    losesStartAtZero = losesStartAtZero + 1;
+    console.log("hello")
+    }       
  };
  function Function2() { 
     $("#secondgreenbox").html(zero + crystalNumber2);
 
     console.log($("#c2").valueAsNumber =crystalNumber2);
     zero = zero + crystalNumber2;
+    if(zero === numberComputerPicked) {
+        zero = 0;
+       $("#winScore").html(winsStartAtZero + 1);
+       winsStartAtZero = winsStartAtZero + 1;
+       console.log("hello")
+
+
+
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+
+        $(".lightgreenbox").html(numberComputerPicked);
+    }
+       if(zero > numberComputerPicked) {
+        ("#loseScore").html(losesStartAtZero + 1);
+         losesStartAtZero = losesStartAtZero + 1;
+         console.log("hello")
+   }
  };
  function Function3() { 
 
@@ -41,12 +90,36 @@ function Function1() {
 
     console.log($("#c3").valueAsNumber =crystalNumber3);
     zero = zero + crystalNumber3;
+    if(zero === numberComputerPicked) {
+        zero = 0;
+       $("#winScore").html(winsStartAtZero + 1);
+       winsStartAtZero = winsStartAtZero + 1;
+       console.log("hello")
+    }
+       if(zero > numberComputerPicked) {
+        ("#loseScore").html(losesStartAtZero + 1);
+         losesStartAtZero = losesStartAtZero + 1;
+         console.log("hello")
+   }
  };
  function Function4() { 
 
     $("#secondgreenbox").html(zero + crystalNumber4);
 
-    console.log($("#c4").valueAsNumber =crystalNumber4);
+    console.log($("#c4").valueAsNumber = crystalNumber4);
     zero = zero + crystalNumber4;
+    if(zero === numberComputerPicked) {
+        zero = 0;
+       $("#winScore").html(winsStartAtZero + 1);
+       winsStartAtZero = winsStartAtZero + 1;
+       console.log("hello")
+    }
+       if(zero > numberComputerPicked) {
+        ("#loseScore").html(losesStartAtZero + 1);
+         losesStartAtZero = losesStartAtZero + 1;
+         console.log("hello")
+       
+   }
  };
 
+ 
