6f25cd463f7503535012aea0afd45690067bd7d4
Bryan Liu <bryanliu@MacBook-Pro.local>
2018-11-14 09:53:08 -0800

    format html, js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,241 +9,241 @@ var crystalNumber1 = Math.floor((Math.random() * 12) + 1);
 var crystalNumber2 = Math.floor((Math.random() * 12) + 1);
 var crystalNumber3 = Math.floor((Math.random() * 12) + 1);
 var crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
+$("#c1").valueAsNumber = crystalNumber1;
+$("#c2").valueAsNumber = crystalNumber2;
+$("#c3").valueAsNumber = crystalNumber3;
+$("#c4").valueAsNumber = crystalNumber4;
+console.log($("#c1").valueAsNumber = crystalNumber1);
+console.log($("#c2").valueAsNumber = crystalNumber2);
+console.log($("#c3").valueAsNumber = crystalNumber3);
+console.log($("#c4").valueAsNumber = crystalNumber4);
 
 var zero = 0;
 var winsStartAtZero = 0;
 var losesStartAtZero = 0;
 //buttons value appear in dark green box and add
 
-function Function1() { 
+function Function1() {
 
     $("#secondgreenbox").text(zero + crystalNumber1);
 
-    console.log($("#c1").valueAsNumber =crystalNumber1);
+    console.log($("#c1").valueAsNumber = crystalNumber1);
 
     zero = zero + crystalNumber1;
 
-    if(zero === numberComputerPicked) {
+    if (zero === numberComputerPicked) {
         zero = 0;
-       $("#winScore").html(winsStartAtZero + 1);
-       winsStartAtZero = winsStartAtZero + 1;
-       console.log("hello")
+        $("#winScore").html(winsStartAtZero + 1);
+        winsStartAtZero = winsStartAtZero + 1;
+        console.log("hello")
 
 
-       numberComputerPicked = Math.floor((Math.random() * 102) + 19);
-        
-       $(".lightgreenbox").text(numberComputerPicked);
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
     }
-    if(zero > numberComputerPicked) {
-   $("#loseScore").text(losesStartAtZero + 1);
-    losesStartAtZero = losesStartAtZero + 1;
-    console.log("hello")
-    zero = 0;
+    if (zero > numberComputerPicked) {
+        $("#loseScore").text(losesStartAtZero + 1);
+        losesStartAtZero = losesStartAtZero + 1;
+        console.log("hello")
+        zero = 0;
 
-    numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").text(numberComputerPicked);
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
-    }       
- };
- function Function2() { 
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
+    }
+};
+function Function2() {
     $("#secondgreenbox").text(zero + crystalNumber2);
 
-    console.log($("#c2").valueAsNumber =crystalNumber2);
+    console.log($("#c2").valueAsNumber = crystalNumber2);
     zero = zero + crystalNumber2;
-    if(zero === numberComputerPicked) {
+    if (zero === numberComputerPicked) {
         zero = 0;
-       $("#winScore").text(winsStartAtZero + 1);
-       winsStartAtZero = winsStartAtZero + 1;
-       console.log("hello")
+        $("#winScore").text(winsStartAtZero + 1);
+        winsStartAtZero = winsStartAtZero + 1;
+        console.log("hello")
 
 
 
-       numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").text(numberComputerPicked);
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
     }
-       if(zero > numberComputerPicked) {
+    if (zero > numberComputerPicked) {
         $("#loseScore").text(losesStartAtZero + 1);
-         losesStartAtZero = losesStartAtZero + 1;
-         console.log("hello")
-         zero = 0;
+        losesStartAtZero = losesStartAtZero + 1;
+        console.log("hello")
+        zero = 0;
 
 
-         numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").text(numberComputerPicked);
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
-   }
- };
- function Function3() { 
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
+    }
+};
+function Function3() {
 
     $("#secondgreenbox").text(zero + crystalNumber3);
 
-    console.log($("#c3").valueAsNumber =crystalNumber3);
+    console.log($("#c3").valueAsNumber = crystalNumber3);
     zero = zero + crystalNumber3;
-    if(zero === numberComputerPicked) {
+    if (zero === numberComputerPicked) {
         zero = 0;
-       $("#winScore").text(winsStartAtZero + 1);
-       winsStartAtZero = winsStartAtZero + 1;
-       console.log("hello")
+        $("#winScore").text(winsStartAtZero + 1);
+        winsStartAtZero = winsStartAtZero + 1;
+        console.log("hello")
 
-       numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").text(numberComputerPicked);
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
     }
-       if(zero > numberComputerPicked) {
+    if (zero > numberComputerPicked) {
         $("#loseScore").text(losesStartAtZero + 1);
-         losesStartAtZero = losesStartAtZero + 1;
-         console.log("hello")
+        losesStartAtZero = losesStartAtZero + 1;
+        console.log("hello")
 
 
-         zero = 0;
-         numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        zero = 0;
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").text(numberComputerPicked);
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
-   }
- };
- function Function4() { 
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
+    }
+};
+function Function4() {
 
     $("#secondgreenbox").text(zero + crystalNumber4);
 
     console.log($("#c4").valueAsNumber = crystalNumber4);
     zero = zero + crystalNumber4;
-    if(zero === numberComputerPicked) {
+    if (zero === numberComputerPicked) {
         zero = 0;
-       $("#winScore").html(winsStartAtZero + 1);
-       winsStartAtZero = winsStartAtZero + 1;
-       console.log("hello")
+        $("#winScore").html(winsStartAtZero + 1);
+        winsStartAtZero = winsStartAtZero + 1;
+        console.log("hello")
 
 
-       numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").html(numberComputerPicked);
+        $(".lightgreenbox").html(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
     }
-       if(zero > numberComputerPicked) {
+    if (zero > numberComputerPicked) {
         $("#loseScore").text(losesStartAtZero + 1);
-         losesStartAtZero = losesStartAtZero + 1;
-         console.log("hello")
+        losesStartAtZero = losesStartAtZero + 1;
+        console.log("hello")
 
-         zero = 0;
-         numberComputerPicked = Math.floor((Math.random() * 102) + 19);
+        zero = 0;
+        numberComputerPicked = Math.floor((Math.random() * 102) + 19);
 
-       $(".lightgreenbox").text(numberComputerPicked);
+        $(".lightgreenbox").text(numberComputerPicked);
 
         crystalNumber1 = Math.floor((Math.random() * 12) + 1);
- crystalNumber2 = Math.floor((Math.random() * 12) + 1);
- crystalNumber3 = Math.floor((Math.random() * 12) + 1);
- crystalNumber4 = Math.floor((Math.random() * 12) + 1);
-$("#c1").valueAsNumber =crystalNumber1;
-$("#c2").valueAsNumber =crystalNumber2;
-$("#c3").valueAsNumber =crystalNumber3;
-$("#c4").valueAsNumber =crystalNumber4;
-console.log($("#c1").valueAsNumber =crystalNumber1);
-console.log($("#c2").valueAsNumber =crystalNumber2);
-console.log($("#c3").valueAsNumber =crystalNumber3);
-console.log($("#c4").valueAsNumber =crystalNumber4);
+        crystalNumber2 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber3 = Math.floor((Math.random() * 12) + 1);
+        crystalNumber4 = Math.floor((Math.random() * 12) + 1);
+        $("#c1").valueAsNumber = crystalNumber1;
+        $("#c2").valueAsNumber = crystalNumber2;
+        $("#c3").valueAsNumber = crystalNumber3;
+        $("#c4").valueAsNumber = crystalNumber4;
+        console.log($("#c1").valueAsNumber = crystalNumber1);
+        console.log($("#c2").valueAsNumber = crystalNumber2);
+        console.log($("#c3").valueAsNumber = crystalNumber3);
+        console.log($("#c4").valueAsNumber = crystalNumber4);
 
 
-       
-   }
- };
 
- 
+    }
+};
+
+
